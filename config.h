696124aee4ef431e40bb47c29c28ac4ca861c1b2
Sergei Grechanik <sergei.grechanik@gmail.com>
2021-11-25 06:53:51 -0800

    [MY] Ctrl+Shift+RightClick to show img in feh

--- config.h
+++ config.h
@@ -196,6 +196,7 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ ControlMask|ShiftMask,Button3, previewimage,   {.s = "feh"} },
 };
 
 /* Internal keyboard shortcuts. */
