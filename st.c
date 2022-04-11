37cb1d946b6d1850f9797c073d5a52f9aa266ff9
wael <40663@protonmail.com>
2022-04-11 17:04:30 +0300

    [PATCH] boxdraw
    See https://st.suckless.org/patches/boxdraw/
    
    The applied patch is:
    https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.5.diff

--- st.c
+++ st.c
@@ -1215,6 +1215,9 @@ tsetchar(Rune u, const Glyph *attr, int x, int y)
 	term.dirty[y] = 1;
 	term.line[y][x] = *attr;
 	term.line[y][x].u = u;
+
+	if (isboxdraw(u))
+		term.line[y][x].mode |= ATTR_BOXDRAW;
 }
 
 void
