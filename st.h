85cebfe76bcef37c70da6e7f1689f4f258fde00a
wael <40663@protonmail.com>
2022-04-11 17:14:06 +0300

    [PATCH] glyph wide support
    https://st.suckless.org/patches/glyph_wide_support/
    https://st.suckless.org/patches/glyph_wide_support/st-glyph-wide-support-boxdraw-20220411-ef05519.diff

--- st.h
+++ st.h
@@ -43,6 +43,12 @@ enum glyph_attribute {
 	ATTR_IMAGE      = 1 << 14,
 };
 
+enum drawing_mode {
+    DRAW_NONE = 0,
+    DRAW_BG = 1 << 0,
+    DRAW_FG = 1 << 1,
+};
+
 enum selection_mode {
 	SEL_IDLE = 0,
 	SEL_EMPTY = 1,
