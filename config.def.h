cb9e9af5cac901d5da6d6b0bb5d23f68e01dea23
Sergei Grechanik <sergei.grechanik@gmail.com>
2023-12-25 09:58:12 -0800

    [Graphics] Improve old image unloading/deletion
    - Now there is an additional excess threshold that's added to the limit:
      the unloading/deletion starts only when a metric is over the limit
      plus this excess threshold (5% by default), and it continues until the
      metric is below the limit.
    
    - Unloading/deletion now sorts images and placements by atime first,
      so it's not quadratic anymore.
    
    - There is now a limit on the number of images/placements.
    
    - When cleaning up ram, we try to first unload images and then
      placements. I'm not sure this is the right approach, but images should
      rarely be needed, unlike placements.

--- config.def.h
+++ config.def.h
@@ -171,13 +171,18 @@ static unsigned int defaultattr = 11;
 /// The template for the cache directory.
 const char graphics_cache_dir_template[] = "/tmp/st-images-XXXXXX";
 /// The max size of a single image file, in bytes.
-unsigned graphics_max_image_size = 20 * 1024 * 1024;
+unsigned graphics_max_single_image_file_size = 20 * 1024 * 1024;
 /// The max size of the cache, in bytes.
 unsigned graphics_total_cache_size = 300 * 1024 * 1024;
 /// The max ram size of an image or placement, in bytes.
-unsigned graphics_max_image_ram_size = 100 * 1024 * 1024;
+unsigned graphics_max_single_image_ram_size = 100 * 1024 * 1024;
 /// The max total size of all images loaded into RAM.
 unsigned graphics_max_total_ram_size = 300 * 1024 * 1024;
+/// The max total number of image placements and images.
+unsigned graphics_max_total_placements = 4096;
+/// The ratio by which limits can be exceeded. This is to reduce the frequency
+/// of image removal.
+double graphics_excess_tolerance_ratio = 0.05;
 
 /*
  * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
