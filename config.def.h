dfd46657304becce171a45c4960284dd46611bd8
Sergei Grechanik <sergei.grechanik@gmail.com>
2024-01-15 14:41:05 -0800

    [CONFIG] Enable boxdraw by default for testing

--- config.def.h
+++ config.def.h
@@ -73,8 +73,8 @@ static unsigned int cursorthickness = 2;
  *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
  * 0: disable (render all U25XX glyphs normally from the font).
  */
-const int boxdraw = 0;
-const int boxdraw_bold = 0;
+const int boxdraw = 1;
+const int boxdraw_bold = 1;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
 const int boxdraw_braille = 0;
