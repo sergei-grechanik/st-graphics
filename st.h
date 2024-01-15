3eb8c9b9f3c28c6bd0658cf1dee24ce09abfc3d6
Sergei Grechanik <sergei.grechanik@gmail.com>
2024-01-15 15:26:23 -0800

    [PATCH] scrollback
    See https://st.suckless.org/patches/scrollback/
    
    The patch:
    https://st.suckless.org/patches/scrollback/st-scrollback-ringbuffer-0.8.5.diff

--- st.h
+++ st.h
@@ -19,6 +19,7 @@
 
 #define TRUECOLOR(r,g,b)	(1 << 24 | (r) << 16 | (g) << 8 | (b))
 #define IS_TRUECOL(x)		(1 << 24 & (x))
+#define HISTSIZE            2000
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
--- x.c
+++ x.c
@@ -59,6 +59,8 @@ static void zoom(const Arg *);
 static void zoomabs(const Arg *);
 static void zoomreset(const Arg *);
 static void ttysend(const Arg *);
+void kscrollup(const Arg *);
+void kscrolldown(const Arg *);
 
 /* config.h for applying patches and the configuration. */
 #include "config.h"
